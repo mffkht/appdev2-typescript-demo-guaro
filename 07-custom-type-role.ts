8592c24024eaa562e0f7177ed98d64f2284de9f6
maffiakhateguaro <maffiakhateg@gmail.com>
2026-03-27 01:02:40 +0800

    Done playing and updating the 07-custom-type-role.ts

--- 07-custom-type-role.ts
+++ 07-custom-type-role.ts
@@ -1,7 +1,7 @@
 (() => {
     // Type Aliases or Custom Types
     // use `type` keyword to create custom type
-    type Role = 'admin' | 'guess' | 'teacher' | 'student'
+    type Role = 'admin' | 'guess' | 'Principal' | 'student'
 
     // Complex Object Type Definition
     type User = {
